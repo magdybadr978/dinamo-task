dabea2b2b352de5992cff9c1fcd6cdc99f24f525
magdybadr978 <magdibadr2002@gmail.com>
2024-09-29 19:44:17 +0300

    fix: attach data in modules consumer

--- src/adminDashboard/consumer/consumer.modules.ts
+++ src/adminDashboard/consumer/consumer.modules.ts
@@ -1,6 +1,9 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Consumer, consumerSchema } from "src/models/consumers/consumer.schema";
+import { ConsumerController } from "./consumer.controller";
+import { consumerService } from "./consumer.service";
+import { ConsumerRepository } from "src/models/consumers/consumer.repository";
 
 
 
@@ -10,9 +13,9 @@ import { Consumer, consumerSchema } from "src/models/consumers/consumer.schema";
       { name : Consumer.name , schema : consumerSchema}
     ])
   ],
-  controllers : ,
-  providers : ,
-  exports : 
+  controllers : [ConsumerController],
+  providers : [consumerService , ConsumerRepository],
+   exports : [consumerService , ConsumerRepository]
 })
 
 export class ConsumerModule {}
