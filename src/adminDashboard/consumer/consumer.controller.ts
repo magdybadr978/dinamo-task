2a1a8fa0a0577271e804c9687584a685c76b91d1
magdybadr978 <magdibadr2002@gmail.com>
2024-09-29 19:44:47 +0300

    fix : make controllers for consumer

--- src/adminDashboard/consumer/consumer.controller.ts
+++ src/adminDashboard/consumer/consumer.controller.ts
@@ -1,10 +1,64 @@
-import { Controller } from "@nestjs/common";
-import { Consumer } from "src/models/consumers/consumer.schema";
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { consumerService } from './consumer.service';
 
 @Controller('/daschBoard/admin')
 export class ConsumerController {
-  constructor(private readonly consumerService : Consumer) {
-    
+  constructor(private readonly consumerService: consumerService) {}
+
+  @Post('add')
+  async createConsumer(
+    @Body()
+    createConsumerDTO: {
+      name: string;
+      phone: string;
+      password: string;
+    },
+  ) {
+    return this.consumerService.createConsumer(
+      createConsumerDTO.name,
+      createConsumerDTO.phone,
+      createConsumerDTO.password,
+    );
+  }
+
+  @Get('getAll')
+  async getAllConsumers() {
+    return this.consumerService.getAllConsumers();
+  }
+
+  @Get('getSpecific/:id')
+  async getSpecificConsumer(@Param('id') id: string) {
+    return this.consumerService.getConsumerById(id);
+  }
+
+  @Put('updateConsumer/:id')
+  async updateConsumer(
+    @Param('id') id: string,
+    @Body()
+    updateConsumerDTO: {
+      newName: string;
+      newPhone: string;
+      newPassword: string;
+    },
+  ) {
+    return this.consumerService.updateConsumer(
+      id,
+      updateConsumerDTO.newName,
+      updateConsumerDTO.newPhone,
+      updateConsumerDTO.newPassword,
+    );
+  }
+
+  @Delete('deleteConsumer/:id')
+  async deleteConsumer(@Param('id') id: string) {
+    return this.consumerService.deleteConsumer(id);
   }
 }
