1e4e17dc68921b27ed5ec6c58c036357523d22e3
magdybadr978 <magdibadr2002@gmail.com>
2024-09-29 12:35:49 +0300

    make category controller

--- src/adminDashboard/category/category.controller.ts
+++ src/adminDashboard/category/category.controller.ts
@@ -1,6 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { CategoryService } from './category.service';
 
-@Controller()
+@Controller('categories')
 export class CategoryController {
-  constructor() {}
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Post()
+  async createCategory(@Body('name') name: string) {
+    return this.categoryService.createCategory(name);
+  }
+
+  @Get()
+  async getAllCategories() {
+    return this.categoryService.getAllCategories();
+  }
+
+  @Get(':id')
+  async getCategoryByName(@Param('id') id: string) {
+    return this.categoryService.getCategoryById(id);
+  }
+
+  @Put(':id')
+  async updateCategory(
+    @Param('id') id: string,
+    @Body('newName') newName: string,
+  ) {
+    return this.categoryService.updateCategory(id, newName);
+  }
+
+  @Delete(':id')
+  async deleteCategory(@Param('id') id: string) {
+    return this.categoryService.deleteCategory(id);
+  }
 }
