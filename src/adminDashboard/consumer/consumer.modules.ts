cd7fab7e90db5cdc325d9b044054113b5185e8b7
magdybadr978 <magdibadr2002@gmail.com>
2024-09-30 13:57:09 +0300

    fix: add discriminators

--- src/adminDashboard/consumer/consumer.modules.ts
+++ src/adminDashboard/consumer/consumer.modules.ts
@@ -1,21 +1,23 @@
-import { Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
-import { Consumer, consumerSchema } from "src/models/consumers/consumer.schema";
-import { ConsumerController } from "./consumer.controller";
-import { consumerService } from "./consumer.service";
-import { ConsumerRepository } from "src/models/consumers/consumer.repository";
-
-
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Consumer, consumerSchema } from 'src/models/consumers/consumer.schema';
+import { ConsumerController } from './consumer.controller';
+import { consumerService } from './consumer.service';
+import { ConsumerRepository } from 'src/models/consumers/consumer.repository';
+import { User, userSchema } from 'src/models/common/user.schema';
 
 @Module({
-  imports : [
+  imports: [
     MongooseModule.forFeature([
-      { name : Consumer.name , schema : consumerSchema}
-    ])
+      {
+        name: User.name,
+        schema: userSchema,
+        discriminators: [{ name: Consumer.name, schema: consumerSchema }],
+      },
+    ]),
   ],
-  controllers : [ConsumerController],
-  providers : [consumerService , ConsumerRepository],
-   exports : [consumerService , ConsumerRepository]
+  controllers: [ConsumerController],
+  providers: [consumerService, ConsumerRepository],
+  exports: [consumerService, ConsumerRepository],
 })
-
 export class ConsumerModule {}
